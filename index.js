fca4b348a015b712e32037018e8506d93a0d166c
MinIzL <minilekz@gmail.com>
2023-03-13 15:13:43 +0700

    Completed HTML,CSS

---
+++ index.js
@@ -0,0 +1,8 @@
+function step2() {
+  var x = document.getElementById("step_2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
